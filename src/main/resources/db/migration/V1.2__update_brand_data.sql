9e78edb28c83dde50e1f78904b622ca9d428c29c
SonDongHwan <sdh549000@gmail.com>
2024-12-23 17:33:29 +0900

    feat: brand 데이터 추가

---
+++ src/main/resources/db/migration/V1.2__update_brand_data.sql
@@ -0,0 +1,7 @@
+UPDATE brand
+SET `name` = '3M'
+WHERE `sn` = 2;
+
+UPDATE brand
+SET `name` = '현대'
+WHERE `sn` = 3;
---
+++ src/main/resources/db/migration/V1.3__insert_brand_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO brand
+VALUES (null, 'LX', 'https://www.lxzin.com/'), (null, '예림', 'https://www.yerim.net/kor/main/main.html'), (null, '삼성', 'http://www.samsungfilm.co.kr/?lang=Ko');
